f2b9333475e2e09bd5bb277abe83ecc1032adf58
Ericlecio Thiago <101409162+Ericlecio@users.noreply.github.com>
2024-07-16 20:52:08 -0300

    .

---
+++ js/cadastro_paciente.js
@@ -0,0 +1,120 @@
+function senha() {
+    var passwordInput = document.getElementById("passwordInput");
+    var togglePassword = document.querySelector(".toggle-password");
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        togglePassword.textContent = "Ocultar Senha";
+    } else {
+        passwordInput.type = "password";
+        togglePassword.textContent = "Mostrar Senha";
+    }
+}
+
+function confirmaSenha() {
+    var passwordInput = document.getElementById("passwordInput2");
+    var togglePassword = document.querySelector(".toggle-password2");
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        togglePassword.textContent = "Ocultar Senha";
+    } else {
+        passwordInput.type = "password";
+        togglePassword.textContent = "Mostrar Senha";
+    }
+}
+
+document.getElementById("passwordInput2").addEventListener("input", function () {
+    var password1 = document.getElementById("passwordInput").value;
+    var password2 = this.value;
+    var passwordError = document.getElementById("passwordError");
+
+    if (password1 !== password2) {
+        passwordError.textContent = "As senhas não coincidem!";
+    } else {
+        passwordError.textContent = "";
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    var telefoneInput = document.querySelector('input[name="telefone"]');
+
+    telefoneInput.addEventListener('input', function () {
+        var telefone = telefoneInput.value.replace(/\D/g, ''); // Remove caracteres não numéricos
+        var formattedTelefone = formatarTelefone(telefone);
+        telefoneInput.value = formattedTelefone;
+    });
+
+    function formatarTelefone(telefone) {
+        var formatted = telefone;
+
+        // Adiciona o prefixo "9" se o telefone não começar com "9" e tiver 9 dígitos
+        if (telefone.length === 9 && !telefone.startsWith('9')) {
+            formatted = '9' + telefone;
+        }
+
+        // Formatação para (DDD) 99999-9999 ou (DDD) 9999-9999
+        if (telefone.length === 10) {
+            formatted = '(' + telefone.substr(0, 2) + ') ' + telefone.substr(2, 4) + '-' + telefone.substr(6, 4);
+        } else if (telefone.length === 11) {
+            formatted = '(' + telefone.substr(0, 2) + ') ' + telefone.substr(2, 5) + '-' + telefone.substr(7, 4);
+        }
+
+        return formatted;
+    }
+});
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    var avatarInput = document.getElementById('avatarInput');
+    var avatarPreview = document.getElementById('avatarPreview');
+
+    // Quando um arquivo é selecionado
+    avatarInput.addEventListener('change', function () {
+        var file = avatarInput.files[0];
+
+        // Verifica se um arquivo foi selecionado
+        if (file) {
+            var reader = new FileReader();
+
+            // Define o evento de carregamento do arquivo
+            reader.onload = function (e) {
+                avatarPreview.src = e.target.result; // Define o src da imagem para o arquivo carregado
+                avatarPreview.style.display = 'block'; // Exibe a imagem
+            };
+
+            // Carrega o arquivo como URL de dados
+            reader.readAsDataURL(file);
+        } else {
+            avatarPreview.src = 'img/avatar-placeholder.png'; // Mostra o placeholder se nenhum arquivo for selecionado
+            avatarPreview.style.display = 'none'; // Esconde a imagem se nenhum arquivo for selecionado
+        }
+    });
+
+    var telefoneInput = document.querySelector('input[name="telefone"]');
+
+    telefoneInput.addEventListener('input', function () {
+        var telefone = telefoneInput.value.replace(/\D/g, ''); // Remove caracteres não numéricos
+        var formattedTelefone = formatarTelefone(telefone);
+        telefoneInput.value = formattedTelefone;
+    });
+
+    function formatarTelefone(telefone) {
+        var formatted = telefone;
+
+        // Adiciona o prefixo "9" se o telefone não começar com "9" e tiver 9 dígitos
+        if (telefone.length === 9 && !telefone.startsWith('9')) {
+            formatted = '9' + telefone;
+        }
+
+        // Formatação para (DDD) 99999-9999 ou (DDD) 9999-9999
+        if (telefone.length === 10) {
+            formatted = '(' + telefone.substr(0, 2) + ') ' + telefone.substr(2, 4) + '-' + telefone.substr(6, 4);
+        } else if (telefone.length === 11) {
+            formatted = '(' + telefone.substr(0, 2) + ') ' + telefone.substr(2, 5) + '-' + telefone.substr(7, 4);
+        }
+
+        return formatted;
+    }
+});
--- js/navbar.js
+++ js/navbar.js
@@ -1,8 +1,18 @@
+// js/navbar.js
 document.addEventListener("DOMContentLoaded", function() {
     fetch('navbar.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('navbar-placeholder').innerHTML = data;
+
+            // Inicialização do menu hambúrguer
+            const burger = document.getElementById('burger');
+            const navLinks = document.getElementById('nav-links');
+
+            burger.addEventListener('click', () => {
+                navLinks.classList.toggle('nav-active');
+                burger.classList.toggle('toggle');
+            });
         })
         .catch(error => console.error('Error loading navbar:', error));
 });
