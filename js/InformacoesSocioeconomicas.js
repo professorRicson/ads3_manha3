2e96d5b5a64240bc7be10ab21bde70fb8e7f1562
FlavioWallefy1 <fwso@discente.ifpe.edu.br>
2024-07-17 21:58:28 -0300

    Criando tela informação e socieconomica

---
+++ js/InformacoesSocioeconomicas.js
@@ -0,0 +1,28 @@
+function confirmaSenha() {
+    var passwordInput = document.getElementById("passwordInput2");
+    var togglePassword = document.querySelector(".toggle-password2");
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        togglePassword.textContent = "Ocultar Senha";
+    } else {
+        passwordInput.type = "password";
+        togglePassword.textContent = "Mostrar Senha";
+    }
+}
+
+document.getElementById("passwordInput2").addEventListener("input", function () {
+    var password1 = document.getElementById("passwordInput").value;
+    var password2 = this.value;
+    var passwordError = document.getElementById("passwordError");
+
+    if (password1 !== password2) {
+        passwordError.textContent = "As senhas não coincidem!";
+    }
+    else if (password1 == password2) {
+        passwordError.textContent = "As senhas se coincidem"
+    }
+    else {
+        passwordError.textContent = "";
+    }
+});
